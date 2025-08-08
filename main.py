82e87fe9d9399194970aae40dcc2ff132fa45cba
s-hammon <stevethebassguy@gmail.com>
2025-08-07 23:59:51 -0500

    added sys.argv params

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import sys
+from typing import List
+
 from stats import word_count, character_freq, char_list
 
 
 def main():
+    fpath = get_file_path(sys.argv)
+    text = get_book_text(fpath)
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
-    text = get_book_text("./books/frankenstein.txt")
     print("----------- Word Count ----------")
     print(f"Found {word_count(text)} total words")
     print("--------- Character Count -------")
@@ -13,6 +17,13 @@ def main():
         if char["char"].isalpha():
             print(f"{char['char']}: {char['num']}")
     print("============= END ===============")
+
+
+def get_file_path(args: List[str]) -> str:
+    if len(args) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return args[1]
 
 
 def get_book_text(fpath: str) -> str:
